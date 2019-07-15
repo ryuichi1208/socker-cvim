3311705bfed6cd3d2c83453e19956997eb47dc52
ryuichi1208 <ryucrosskey@gmail.com>
2019-07-15 11:41:10 +0900

    move repo

---
+++ src/socket.h
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <errno.h>
+#include <stdlib.h>
+
+#define QUEUELIMIT 5
+#define MSGSIZE 1024
+#define BUFSIZE (MSGSIZE + 1)
+#define MAXRECVSTRING 255
+#define MINRECVSTRING 1
