11cafee1fc54de76f610acd466a214ba114d9411
Ryuichi Watanabe <ryucrosskey@gmail.com>
2019-08-01 10:57:20 +0900

    fix dockvim

--- dockvim/dockvim.c
+++ dockvim/dockvim.c
@@ -33,6 +33,7 @@ typedef unsigned long long int u_64;
 #endif
 
 typedef struct namespace {
+    int fd;
     int pid;
     char *type;
 } namespace;
@@ -55,10 +56,11 @@ int get_container_pid(char *container_name)
     char pid_buf[16];
     struct stat stat_buf;
 
-    sprintf(dock_cmd, "docker container inspect %s --format={{.Id}}", container_name);
+    sprintf(dock_cmd, "docker container inspect %s --format={{.State.Pid}}", container_name);
     sprintf(tmp_file, "/tmp/%s_%d.tmp", container_name, getpid());
     sprintf(exec_cmd, "%s > %s", dock_cmd, tmp_file);
 
+    // コンテナのプロセスIDを取得し、tmp配下へ書き出す
     ret = system(exec_cmd);
     if (ret != 0)
         err_exit("failed system", errno);
@@ -75,28 +77,51 @@ int get_container_pid(char *container_name)
     if (ret == -1)
         err_exit("failed read", errno);
 
-    return strtol(pid_buf, &endptr, 10);
-    //return ret;
+    pid = strtol(pid_buf, &endptr, 10);
+    return pid;
+}
+
+void set_namespace_info(namespace *n, int pid)
+{
+    // 現在のネームスペースの情報を保持
+    n->pid = getpid();
+    n->type = "mnt";
+
+    // コンテナのネームスペース情報を保持
+    (n+1)->pid = pid;
+    (n+1)->type = "mnt";
 
 }
 
-void open_namespace(namespace *n)
+void open_namespace(namespace *n, char *container_name)
 {
     u_16 i;
+    u_32 pid;
     int fd;
     char procfile[2][64];
 
+    // 対象のコンテナのPIDを取得する
+    pid = get_container_pid(container_name);
+
+    set_namespace_info(n, pid);
+
     for (i = 0; i < 2; i++) {
-        sprintf(*(procfile+i), "/prot/%d/ns/%s", (n+1)->pid, (n+i)->type);
+        sprintf(*(procfile+i), "/proc/%d/ns/%s", (n+i)->pid, (n+i)->type);
+        printf("Open namespaces file : %s\n", (procfile+i));
+
+        // ネームスペースを保持しておく
+        (n+i)->fd = open(*(procfile+i), O_RDONLY|O_EXCL);
+        if ((n+i)->fd == -1)
+            err_exit("failed open", errno);
     }
 }
 
-void checl_enviroment()
+void check_enviroment()
 {
     int ret;
     struct utsname s;
 
-	ret = uname(&s);
+    ret = uname(&s);
     if (ret) {
         err_exit("failed uname", errno);
     }
@@ -109,18 +134,24 @@ void checl_enviroment()
 int main(int argc, char **argv)
 {
     u_16 i;
-    namespace *n = malloc(sizeof(namespace) * 2);
+    u_32 pid;
+    char *container_name;
+    char *filepath;
+    namespace *n;
 
-    checl_enviroment();
+    //check_enviroment();
 
-    for (i = 0; i < 2; i++) {
-        (n+i)->pid = 20;
-        (n+i)->type = "abs";
+    if (argc != 2) {
+        fprintf(stderr, "Usage : %s [CONTAINERNAME|CONTAINERID] filepath\n");
+        exit(1);
     }
-    open_namespace(n);
 
-    int pid = get_container_pid("nginx_1");
-    printf("%d\n", pid);
+    container_name = argv[1];
+    filepath 	  = argv[2];
+
+    // 対象コンテナのネームスペース情報を取得する
+    n = malloc(sizeof(namespace) * 2);
+    open_namespace(n, container_name);
 
     return 0;
 }
